50e444bf7df33ced87f9e760533ba63468706454
hhunterzinck <haley.hunterzinck@sagebase.org>
2021-11-20 00:54:23 +0000

    add Prostate class

--- geniesp/bpc_config.py
+++ geniesp/bpc_config.py
@@ -81,3 +81,21 @@ class Panc(BpcProjectRunner):
         # "tree/a672a70ad5195e6e8359325f5cea10bef384b2ff/"
         # "geniesp/bpc_config.py"
     )
+
+class Prostate(BpcProjectRunner):
+    """NSCLC BPC sponsored project"""
+    # Sponsorted project name
+    _SPONSORED_PROJECT = 'Prostate'
+    # Redcap codes to cbioportal mapping synid and form key is in
+    _REDCAP_TO_CBIOMAPPING_SYNID = "syn25712693"
+    # Mapping from Synapse Table to form (derived files)
+    _DATA_TABLE_IDS = "syn22296821"
+    # Storage of not found samples
+    _SP_REDCAP_EXPORTS_SYNID = "syn21446571"
+    # Run `git rev-parse HEAD` in Genie_processing directory to
+    # obtain shadigest
+    _GITHUB_REPO = (
+        "https://github.com/Sage-Bionetworks/GENIE-Sponsored-Projects/"
+        # "tree/a672a70ad5195e6e8359325f5cea10bef384b2ff/"
+        # "geniesp/bpc_config.py"
+    )
