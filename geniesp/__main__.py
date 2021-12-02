5976b4efe38978a5943362cfd5bfa52aaf5c91b8
Haley Hunter-Zinck <haley.hunterzinck@sagebase.org>
2021-12-02 14:59:18 -0800

    adapt cli to pass synapse credential file

--- geniesp/__main__.py
+++ geniesp/__main__.py
@@ -21,20 +21,28 @@ def main():
     parser = argparse.ArgumentParser(
         description='Run GENIE sponsored projects'
     )
-    parser.add_argument("sp", type=str,
+    parser.add_argument('--sp', dest='sp', type=str,
                         help='Specify sponsored project to run',
                         choices=BPC_MAPPING.keys())
-    parser.add_argument(
-        "cBioPath", type=str,
+    
+    parser.add_argument('--release', dest = 'release', type=str,
+                        help='Specify bpc release')
+    parser.add_argument('--cbiopath',
+        dest='cBioPath', type=str,
         help='Specify path to cbio: must do '
              '`git clone https://github.com/cBioPortal/cbioportal.git`'
     )
-    parser.add_argument("release", type=str,
-                        help='Specify bpc release')
+    parser.add_argument('--synconfigpath',
+        dest='synConfigPath', type=str,
+        help='Path to .synapseConfig file'
+    )
     parser.add_argument("--staging", action='store_true',
                         help="If true, files aren't uploaded onto synapse")
     args = parser.parse_args()
-    syn = synapseclient.login()
+
+    syn = synapseclient.Synapse(configPath = args.synconfigpath)
+    syn.login()
+
 
     BPC_MAPPING[args.sp](syn, args.cBioPath, release=args.release,
                          staging=args.staging).run()
