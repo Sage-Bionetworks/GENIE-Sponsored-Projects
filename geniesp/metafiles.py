d9f648d2a557f548283f8c898a2911a95b70cabb
thomasyu888 <thomas.yu@sagebase.org>
2022-02-14 18:39:59 +0800

    Add genomic metadata file creation

--- geniesp/metafiles.py
+++ geniesp/metafiles.py
@@ -32,17 +32,17 @@ def create_meta_file(
         alteration_type (str): Alteration type
 
     Returns:
-        dict: metafile in a dict
+        dict: cBioPortal meta file
     """
     # datatype is SAMPLE_ATTRIBUTE or PATIENT_ATTRIBUTE
-    clinical_meta = {
+    meta_info = {
         "cancer_study_identifier": study_identifier,
         "genetic_alteration_type": alteration_type,
         "datatype": datatype,
         "data_filename": filename
     }
-    write_meta_file(clinical_meta, filename)
-    return clinical_meta
+    write_meta_file(meta_info, filename)
+    return meta_info
 
 
 def create_genomic_meta_file(
@@ -54,36 +54,54 @@ def create_genomic_meta_file(
     profile_description: str,
     filename: str
 ) -> dict:
-    meta_cna = {
-        "cancer_study_identifier": "crc_genie_bpc",
-        "genetic_alteration_type": "COPY_NUMBER_ALTERATION",
-        "datatype": "DISCRETE",
-        "stable_id": "cna",
-        "show_profile_in_analysis_tab": "true",
-        "profile_name": "Copy-number alterations",
-        "profile_description": "Copy-number alterations",
-        "data_filename": "data_CNA.txt"
-    }
+    """Create genomic metadata files
 
+    Args:
+        study_identifier (str): [description]
+        alteration_type (str): [description]
+        datatype (str): [description]
+        stable_id (str): [description]
+        profile_name (str): [description]
+        profile_description (str): [description]
+        filename (str): [description]
 
-def main():
-    """Create metadata files
+    Returns:
+        dict: cBioPortal meta file
     """
-    # Create CRC meta files
+    meta_info = {
+        "cancer_study_identifier": study_identifier,
+        "genetic_alteration_type": alteration_type,
+        "datatype": datatype,
+        "stable_id": stable_id,
+        "show_profile_in_analysis_tab": "true",
+        "profile_name": profile_name,
+        "profile_description": profile_description,
+        "data_filename": filename
+    }
+    write_meta_file(meta_info, filename)
+    return meta_info
+
+
+def main(study_identifier: str):
+    """Create metadata files
+
+    Args:
+        sp (str): cBioPortal study identifier
+    """
     create_meta_file(
-        study_identifier="crc_genie_bpc",
+        study_identifier=study_identifier,
         alteration_type="CLINICAL",
         datatype="SAMPLE_ATTRIBUTE",
         filename="data_clinical_sample.txt"
     )
     create_meta_file(
-        study_identifier="crc_genie_bpc",
+        study_identifier=study_identifier,
         alteration_type="CLINICAL",
         datatype="PATIENT_ATTRIBUTE",
         filename="data_clinical_patient.txt"
     )
     create_meta_file(
-        study_identifier="crc_genie_bpc",
+        study_identifier=study_identifier,
         alteration_type="GENE_PANEL_MATRIX",
         datatype="GENE_PANEL_MATRIX",
         filename="data_gene_matrix.txt"
@@ -99,8 +117,36 @@ def main():
     ]
     for filenames in timeline_meta_files:
         create_meta_file(
-            study_identifier="crc_genie_bpc",
+            study_identifier=study_identifier,
             alteration_type="CLINICAL",
             datatype="TIMELINE",
             filename=filenames
         )
+
+    create_genomic_meta_file(
+        study_identifier=study_identifier,
+        alteration_type="COPY_NUMBER_ALTERATION",
+        datatype="DISCRETE",
+        stable_id="cna",
+        profile_name="Copy-number alterations",
+        profile_description="Copy-number alterations",
+        filename="data_CNA.txt"
+    )
+    create_genomic_meta_file(
+        study_identifier=study_identifier,
+        alteration_type="FUSION",
+        datatype="FUSION",
+        stable_id="fusion",
+        profile_name="Fusions",
+        profile_description="Fusions",
+        filename="data_fusions.txt"
+    )
+    create_genomic_meta_file(
+        study_identifier=study_identifier,
+        alteration_type="MUTATIONS_EXTENDED",
+        datatype="MAF",
+        stable_id="mutations",
+        profile_name="Mutations",
+        profile_description="Mutation data from next-gen sequencing.",
+        filename="data_mutations_extended.txt"
+    )
