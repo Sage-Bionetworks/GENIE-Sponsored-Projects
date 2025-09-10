24afa24741adc252cb918dcc4b4cd34f768a4a34
Chelsea-Na <109613735+Chelsea-Na@users.noreply.github.com>
2025-09-10 15:59:26 -0700

    [GEN-2216] Update bpc_redcap_export_mapping.py
    Updating for the new mapping file

--- geniesp/bpc_redcap_export_mapping.py
+++ geniesp/bpc_redcap_export_mapping.py
@@ -674,7 +674,7 @@ class BpcProjectRunner(metaclass=ABCMeta):
     # Redcap codes to cbioportal mapping synid and form key is in
     # version 38, 42 were last stable version(s)
     # NOTE: Should be pointed towards latest version of table
-    _REDCAP_TO_CBIOMAPPING_SYNID = "syn25712693.60"
+    _REDCAP_TO_CBIOMAPPING_SYNID = "syn25712693.61"
     # Run `git rev-parse HEAD` in Genie_processing directory to obtain shadigest
     _GITHUB_REPO = None
     # Mapping from Synapse Table to derived variables
